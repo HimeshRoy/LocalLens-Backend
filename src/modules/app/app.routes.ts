90924056651e952400318f4dd86a40ca69dcd00f
HimeshRoy <himeshroy.cse@gmail.com>
2026-09-08 01:03:26 +0530

    Test mobile app update

--- src/modules/app/app.routes.ts
+++ src/modules/app/app.routes.ts
@@ -6,13 +6,13 @@ router.get("/version", (_req, res) => {
   res.status(200).json({
     success: true,
     data: {
-  version: "1.0.1",
-  versionCode: 3,
-  apkUrl:
-    "https://github.com/HimeshRoy/locallens-app-apk/releases/latest/download/LocalLens.apk",
-  releaseNotes: "Bug fixes and improvements.",
-  forceUpdate: false,
-},
+      version: "1.0.1",
+      versionCode: 2,
+      apkUrl:
+        "https://github.com/HimeshRoy/locallens-app-apk/releases/latest/download/LocalLens.apk",
+      releaseNotes: "Bug fixes and improvements.",
+      forceUpdate: false,
+    },
   });
 });
 
